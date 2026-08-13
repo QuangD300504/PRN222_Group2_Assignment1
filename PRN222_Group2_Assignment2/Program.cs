f5af28485ed6a15c4620d5612042568647bec776
QuangD300504 <duyq099@gmail.com>
2026-08-13 19:00:00 +0700

    feat(asm2-setup): configure Razor Pages project structure and reference Assignment 1 backend services

--- PRN222_Group2_Assignment2/Program.cs
+++ PRN222_Group2_Assignment2/Program.cs
@@ -1,9 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using PRN222_Group2_Assignment1.Data;
+using PRN222_Group2_Assignment1.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IDocumentService, DocumentService>();
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(8);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
+
+// Ensure DB is created & updated with seeds
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -14,6 +39,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
 app.UseAuthorization();
 
 app.MapRazorPages();
